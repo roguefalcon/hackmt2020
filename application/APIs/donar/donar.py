09b884dba645634f140841097970593d004271c4
Eze Amadi <eze.kachi.amadi@gmail.com>
2020-01-26 09:59:47 +0000

    Created an Endpoint for the Donor API

--- application/APIs/donar/donar.py
+++ application/APIs/donar/donar.py
@@ -11,6 +11,28 @@ def donar_read(id):
     #return in Json format
     return jsonify(data)
 
+
+@main.app.route('/api/1.0/donar/<id>/children', methods=['GET'])
+def donar_details(id):
+
+    #Getting information about the donar
+    g.c.execute('''SELECT * FROM donor WHERE id = ?''',(id))
+    data = g.c.fetchone()
+
+    # Gathering information from Children and Children items
+    g.c.execute('''SELECT c.id, c.age, c.name, c.sex, SUM(i.amount) as total
+     FROM donar_sponsor_children dsc inner join children c
+     on dsc.children_id=c.id left join children_items i
+     on c.id=i.children_id WHERE dsc.donar_id = ? Group by c.id, c.age, 
+     c.name, c.sex''',(id))
+     
+
+     #fetching all data
+    all_data = g.c.fetchall()
+    data["children_info"] = all_data
+    return jsonify(data)
+
+
 #Insert single donor record
 @main.app.route('/api/1.0/donar', methods=['post'])
 def donar_insert():
