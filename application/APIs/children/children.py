b5c34a4ef8865049ed57556da27db32ddc3f4369
albir_code <albir_h@outlook.com>
2020-01-26 01:53:00 +0000

    Fixed some typos in children API.

--- application/APIs/children/children.py
+++ application/APIs/children/children.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, g
+from flask import Flask, render_template, json, request, g, jsonify
 from application import main
 
 @main.app.route('/api/1.0/children/<id>', methods=['GET'])
@@ -14,60 +14,62 @@ def child_read(id):
     return jsonify(data)
 # Update Single child record(Edit)======
 
-@main.app.route('/api/1.0/childern/<id>', methods=['PUT'])
+@main.app.route('/api/1.0/children/<id>', methods=['put'])
 def child_edit(id):
 
+    childrenData = request.json
+
     #child input
-    email = request.form.get('email')
-    age = request.form.get('age')
-    name = request.form.get('name')
-    about_me = request.form.get('about_me')
-    adress = request.form.get('adress')
-    cloth_size = request.form.get('cloth_size')
-    sex = request.form.get('sex')
-    gender = request.form.get('gender')
-    pant_size = request.form.get('pant_size')
-    shoes_size = request.form.get('shoes_size')
-    fav_color = request.form.get('fav_color')
+    email = childrenData['email']
+    age = childrenData['age']
+    name = childrenData['name']
+    about_me = childrenData['about_me']
+    address = childrenData['address']
+    cloth_size = childrenData['cloth_size']
+    sex = childrenData['sex']
+    gender = childrenData['gender']
+    pant_size = childrenData['pant_size']
+    shoes_size = childrenData['shoes_size']
+    fav_color = childrenData['fav_color']
 
-
-   """ if not email or not age or not name or not about_me
+    '''if not email or not name or not name or not about_me
     or not adress or not cloth_size or not sex or not gender
     or not pant_size, or not shoes_size, or not fav_color:
         return jsonify({'success': False, 'error': 
-        'Missing value'}) """
+        'Missing value'}) '''
     # Update the child in the database
     g.c.execute('''
-    UPDATE children set email=?, age=?, name=?, about_me=?
-    adress=?, cloth_size=?, sex=?, gender=?,pant_size,
-    shoes_size=?, fav_color=? WHERE child_id=?''', (
-        email,age,name,about_me, adress, cloth_size, sex,
-        gender, pant_size, shoes_size, fav_color, id
-    )
+    UPDATE children set email=?, name=?, age=?, about_me=?,
+    address=?, cloth_size=?, sex=?, gender=?,pant_size=?,
+    shoes_size=?, fav_color=? WHERE id=?''', (
+        email, name, age, about_me, address, cloth_size, sex, gender, pant_size, shoes_size, fav_color, id
+    ))
+
     g.conn.commit()
     return jsonify({'success': True,  'child_id': id})
 
 
 #Insert Single child record
-@main.app.route('/api/1.0/childern', methods=['post'])
-def child_edit():
+@main.app.route('/api/1.0/children', methods=['post'])
+def child_insert():
 
+    childrenData = request.json
     #child input
-    email = request.form.get('email')
-    age = request.form.get('age')
-    name = request.form.get('name')
-    about_me = request.form.get('about_me')
-    adress = request.form.get('adress')
-    cloth_size = request.form.get('cloth_size')
-    sex = request.form.get('sex')
-    gender = request.form.get('gender')
-    pant_size = request.form.get('pant_size')
-    shoes_size = request.form.get('shoes_size')
-    fav_color = request.form.get('fav_color')
+    email = childrenData['email']
+    age = childrenData['age']
+    name = childrenData['name']
+    about_me = childrenData['about_me']
+    address = childrenData['address']
+    cloth_size = childrenData['cloth_size']
+    sex = childrenData['sex']
+    gender = childrenData['gender']
+    pant_size = childrenData['pant_size']
+    shoes_size = childrenData['shoes_size']
+    fav_color = childrenData['fav_color']
 
     #validation
-   """ if not email or not age or not name or not about_me
-    or not adress or not cloth_size or not sex or not gender
+    """if not email or not age or not name or not about_me
+    or not address or not cloth_size or not sex or not gender
     or not pant_size, or not shoes_size, or not fav_color:
         return jsonify({'success': False, 'error': 
         'Missing value'}) """
@@ -75,20 +77,21 @@ def child_edit():
     # Insert the child in the database
     g.c.execute('''
     INSERT INTO children 
-        (email,age,name,about_me, adress, cloth_size, sex,
+        (email,name, age, about_me, address, cloth_size, sex,
         gender, pant_size, shoes_size, fav_color)
         VALUES(?,?,?,?,?,?,?,?,?,?,?)''',
-    (email,age,name,about_me, adress, cloth_size, sex,
+    (email,name,age,about_me, address, cloth_size, sex,
         gender, pant_size, shoes_size, fav_color
-    )
-        g.conn.commit()
+    ))
+    g.conn.commit()
     # GET THE ID OF THE RECORD JUST CREATED
     rowid = g.c.lastrowid
-    return jsonify({'success': True,  'child_id': id})
+    return jsonify({'success': True,  'child_id': rowid})
 
 
     # Delete Single CHILD REECORD (Delete) ============================================
-@app.route('/api/1.0/children/<id>', methods=['DELETE'])
+
+@main.app.route('/api/1.0/children/<id>', methods=['DELETE'])
 def destination_delete(id):
 
     # DELETE A REC FROM DB
